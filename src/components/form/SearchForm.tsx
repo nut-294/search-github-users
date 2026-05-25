a544bc79719100d2ce62fda3404975f75971189f
Nut-294 <134218655+Nut-294@users.noreply.github.com>
2026-05-25 10:20:15 +0700

    Search Form

--- src/components/form/SearchForm.tsx
+++ src/components/form/SearchForm.tsx
@@ -1,11 +1,42 @@
-type SearchFormProps ={
-userName :string
-setUserName : React.Dispatch<React.SetStateAction<string>>
-}
+import { useState } from "react";
+import { Label } from "../ui/label";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
+import { toast } from "sonner";
 
-const SearchForm = ({userName,setUserName}:SearchFormProps) => {
+type SearchFormProps = {
+  userName: string;
+  setUserName: React.Dispatch<React.SetStateAction<string>>;
+};
+
+const SearchForm = ({ userName, setUserName }: SearchFormProps) => {
+  const [text, setText] = useState(userName);
+  const handleSearch = (e) => {
+    e.preventDefault();
+    if (text === "") {
+      toast("Please enter a valid username");
+      return;
+    }
+    setUserName(text);
+  };
   return (
-    <div>SearchForm</div>
-  )
-}
-export default SearchForm
+    <form
+      onSubmit={handleSearch}
+      className="flex items-center gap-x-2 w-full lg:w-1/3 mb-8"
+    >
+      <Label htmlFor="search" className="sr-only">
+        Search
+      </Label>
+      <Input
+        type="text"
+        id="search"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+        placeholder="Search Github Users..."
+        className="grow bg-background"
+      />
+      <Button type="submit">Search</Button>
+    </form>
+  );
+};
+export default SearchForm;
