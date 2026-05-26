95aa50b40a2aaf7b91623d78c009eeb2fc4382a3
Nut-294 <134218655+Nut-294@users.noreply.github.com>
2026-05-26 10:18:18 +0700

    Apollo Client and Query

---
+++ src/apolloClient.ts
@@ -0,0 +1,41 @@
+import {
+  ApolloClient,
+  InMemoryCache,
+  HttpLink,
+  ApolloLink,
+} from "@apollo/client";
+import { onError } from "@apollo/client/link/error";
+
+//ดัก Error
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+  if (graphQLErrors) {
+    graphQLErrors.forEach(({ message, locations, path }) => {
+      console.error(
+        `[GraphQL error]: Message:${message}, Location:${locations} ,Path:${path}`
+      );
+    });
+  }
+  if (networkError) {
+    console.error(`[Network Error]: ${networkError}`);
+  }
+});
+
+//URL ของ GitHub GraphQL
+const GITHUB_GRAPHQL_API = "https://api.github.com/graphql";
+
+//ช่องทางส่ง request
+const httpLink = new HttpLink({
+  uri: GITHUB_GRAPHQL_API, //ยิง request ที่นี่
+  headers: {
+    Authorization: `Bearer ${import.meta.env.VITE_GITHUB_TOKEN}`,
+  },
+});
+
+const link = ApolloLink.from([errorLink, httpLink]);
+
+const client = new ApolloClient({
+  link,
+  cache: new InMemoryCache(),
+});
+
+export default client;
--- src/components/user/UserProfile.tsx
+++ src/components/user/UserProfile.tsx
@@ -1,8 +1,35 @@
+import { useQuery } from "@apollo/client/react";
+import { GET_USER } from "@/queries";
+import type { UserData } from "@/types";
+
 type UserProfileProps = {
   userName: string;
 };
 
 const UserProfile = ({ userName }: UserProfileProps) => {
-  return <h1 className="text-2xl font-bold">{userName}</h1>;
+  const { data, loading, error } = useQuery<UserData>(GET_USER, {
+    variables: { login: userName },
+  });
+
+  if (loading) return <div>Loading...</div>;
+  if (error) return <h2 className="text-xl">{error.message}</h2>;
+  if (!data) return <h2 className="text-xl">User Not Found</h2>;
+
+  const {
+    avatarUrl,
+    name,
+    bio,
+    url,
+    repositories,
+    followers,
+    following,
+    gists,
+  } = data.user;
+
+  return (
+    <div>
+      <h1 className="text-2xl font-bold">{bio}</h1>
+    </div>
+  );
 };
 export default UserProfile;
